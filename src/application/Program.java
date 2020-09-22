13b0ff9faf09aeaa7a4575bb7a87450102b24c43
SalathielVieira <salathielvieira@outlook.com>
2020-09-22 13:29:03 -0400

    Expressao lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,7 +19,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.00));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		}; // As chaves são opcionais em função com uma única linha.
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}
